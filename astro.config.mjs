45c5b3a88318dbae54d670231f94c332d9b601fa
NabeeLMahfoos <nmkofficial02@gmail.com>
2026-06-09 00:16:18 +0530

    Add senior dev level OpenGraph meta tags and SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://onethree-twoeight.pages.dev',
   integrations: [tailwind()],
   output: 'static',
   build: {
